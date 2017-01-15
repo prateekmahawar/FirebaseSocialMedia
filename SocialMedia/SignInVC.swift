20751de2339fe8a455ecd6fd9105c0477d74cf77
Prateek Mahawar <PrateekMahawar@gmail.com>
2017-01-15 18:59:53 +0530

    InitialCommit

---
+++ SocialMedia/SignInVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialMedia
+//
+//  Created by Prateek Mahawar on 15/01/17.
+//  Copyright © 2017 Prateek Mahawar. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
