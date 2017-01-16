e1a9be230b76f2d1e1ad0c56d61feee9c904d959
Prateek Mahawar <PrateekMahawar@gmail.com>
2017-01-16 22:14:28 +0530

    CustomViewsss

---
+++ SocialMedia/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialMedia
+//
+//  Created by Prateek Mahawar on 16/01/17.
+//  Copyright © 2017 Prateek Mahawar. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY : CGFloat = 120.0 / 255.0
