9a50037deb8e910c84b307a9358839bca8aa6e28
Prateek Mahawar <PrateekMahawar@gmail.com>
2017-01-20 22:57:58 +0530

    Feed UI Complete

--- SocialMedia/CircleView.swift
+++ SocialMedia/CircleView.swift
@@ -1,24 +1,30 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  SocialMedia
 //
-//  Created by Prateek Mahawar on 16/01/17.
+//  Created by Prateek Mahawar on 20/01/17.
 //  Copyright © 2017 Prateek Mahawar. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+        
         
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
+       
+    }
+    
     
 }
