e47d50cbd8b95dda71a0f940548fb7c2588def2e
Prateek Mahawar <PrateekMahawar@gmail.com>
2017-01-21 21:04:06 +0530

    FeedUI

--- SocialMedia/FeedVC.swift
+++ SocialMedia/FeedVC.swift
@@ -10,12 +10,30 @@ import UIKit
 import SwiftKeychainWrapper
 import Firebase
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 338.0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostCellTVC
+        
     }
 
     @IBAction func SignOutBtnTapped(_ sender: Any) {
---
+++ SocialMedia/PostCellTVC.swift
@@ -0,0 +1,27 @@
+//
+//  PostCellTVC.swift
+//  SocialMedia
+//
+//  Created by Prateek Mahawar on 21/01/17.
+//  Copyright © 2017 Prateek Mahawar. All rights reserved.
+//
+
+import UIKit
+
+class PostCellTVC: UITableViewCell {
+    
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+    
+}
