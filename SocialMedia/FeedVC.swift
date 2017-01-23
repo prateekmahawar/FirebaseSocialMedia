c4438b290d7301a61c59211720e7d786518f0222
Prateek Mahawar <PrateekMahawar@gmail.com>
2017-01-23 22:53:30 +0530

    imagePicker Complete

--- SocialMedia/FeedVC.swift
+++ SocialMedia/FeedVC.swift
@@ -10,17 +10,23 @@ import UIKit
 import SwiftKeychainWrapper
 import Firebase
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var imageAdd: CircleView!
     
     var posts = [Post]()
+    var imagePicker: UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
             if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
@@ -63,6 +69,21 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            imageAdd.layer.masksToBounds = true
+            imageAdd.image = image
+            
+        } else {
+            print("FeedVC: Imagepicker problem")
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func addImageTapped(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
     @IBAction func SignOutBtnTapped(_ sender: Any) {
         
         KeychainWrapper.standard.remove(key: KEY_UID)
